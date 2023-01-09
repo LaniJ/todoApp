136f06ff7af0be4603a5338a50db68aeb5635c08
Lani <ooluwalani@gmail.com>
2023-01-08 22:12:54 -0500

    modifications

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -3,8 +3,13 @@ import './Footer.css';
 const Footer = ({ length, deleteAll }) => {
   return ( 
     <div className='footer'>
-      <p>You have <strong>{length}</strong> pending tasks left</p>
-      <button className='delete-all-btn' onClick={deleteAll}>Clear All</button>
+      { length > 0 && <div className='pending-tasks'>
+        <p>You have <strong>{length}</strong> pending task{length > 1 ? 's' : ''} left</p>
+        <button className='delete-all-btn' onClick={deleteAll}>Clear All</button>
+      </div>}
+      { !length  && <div>
+        <p>You have no tasks yet! Add a task.</p>
+      </div>}
     </div>
    );
 }
